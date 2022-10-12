902a2d4bda02bc352a9f3851f854b5975894fe49
GathunguAlex <alex.gathungu@student.moringaschool.com>
2022-10-12 14:38:30 +0300

    proper naming of components

---
+++ src/components/Add dish.js
@@ -0,0 +1,18 @@
+export default function AddDish() {
+    return 
+    <form>
+      <div>
+        <label>food</label>
+        <input type="text" name="food" />
+      </div>
+      <div>
+        <label>image</label>
+        <input type="text" name="image" />
+      </div>
+
+      <div>
+        <label>description</label>
+        <input type="text" name="description" />
+      </div>
+    </form>;
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../App.css';
+import AddDish from './Add dish';
 //import Navbar from './nav';
 
 
