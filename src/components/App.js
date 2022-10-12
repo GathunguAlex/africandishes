791d91abc8c821715ed06028cb3d355b2a69fb18
GathunguAlex <alex.gathungu@student.moringaschool.com>
2022-10-12 15:36:06 +0300

    setting up remove dish component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React,{useState} from 'react';
 import '../App.css';
 import AddDish from './Add dish';
 import Footer from './Footer';
 import Nav from './Nav';
+import Display from './Display dish';
+import RemoveDish from './Remove dish';
+
+const allFoods = ['all'. ...new Set(dishes.map(dishes) => dishes.food)];
 
 function App() {
+  const [addDish, setAddDish] = useState(dishes)
+  const [removeDish,setRemoveDish]=useState("")
   return 
   
 };
