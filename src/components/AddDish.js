72e38e91bf66e28994ebf2dc4eda4b09ed77467a
GathunguAlex <alex.gathungu@student.moringaschool.com>
2022-10-12 19:49:20 +0300

    completed assignment

--- src/components/AddDish.js
+++ src/components/AddDish.js
@@ -1,22 +1,28 @@
-import React,{useState} from "react"
-export default function AddDish({onSubmit}) {
-  const [food, setFood] = useState({ name: "", description: "" });  ;
+import React, { useState } from "react"
+import { Link,Routes,Route } from "react-router-dom";
+
+
+
+export default function AddDish() {
+  const [food, setFood] = useState({ name: "", description: "" });;
+  
+
   function handleSubmit(e) {
     e.preventDefault();
-    onSubmit(food);
-    setFood({ name: "", description:""});
+    
+    setFood({ name: "", description: "" });
 
-};  
+    fetch("http://localhost:3000/foods", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(food),
+    })
+      .then((r) => r.json())
+      .then((newFood) => console.log(newFood));
 
-  fetch("http://localhost:3000/foods", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(),
-  })
-    .then((r) => r.json())
-    .then((newFood) => onSubmit(newFood));
+  };
 
   function handleChange(event) {
     setFood({
@@ -27,26 +33,31 @@ export default function AddDish({onSubmit}) {
 
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form >
       <div>
-        <label>name</label>
-        <input onChange={handleChange} value={food.name} type="text" name="food" />
+        <label>food</label>
+        <input onChange={handleChange}type="text" name="name" />
       </div>
-      <div>
-        <label>image</label>
-        <input onChange={handleChange} type="text" name="image" />
-      </div>
-
+      
       <div>
         <label>description</label>
         <input
           onChange={handleChange}
-          value={food.description}
+          
           type="text"
           name="description"
         />
       </div>
-      <button type="button" >Add Food</button>
-    </form>
+      <Link to={"./DisplayDish.js"}><button onDoubleClick={() => {
+  
+      
+        <Routes>
+          <Route path="./DisplayDish.js" />
+        </Routes>
+      }}
+
+        onClick={handleSubmit}  type="button" >Add Food</button>
+      </Link>
+      </form>
   );
 }
